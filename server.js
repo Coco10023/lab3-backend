b1a0be0125b5920827d5a665cbb82ea7a22bb7ca
Coco10023 <mauricio2003@live.se>
2026-05-07 09:21:14 +0200

    skriver kod på server.js för att kunna koppla till MongoDB databasen

---
+++ server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Ansluten till MongoDB"))
+  .catch(err => console.log(err));
+
+  app.get("/", (req, res) => {
+  res.json({ message: "API fungerar" });
+});
+
+app.listen(3000, () => {
+  console.log("Servern körs på port 3000");
+});
